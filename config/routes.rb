b4ed6820bf7473cb791722800c70a562b09426ba
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2022-10-08 03:12:01 +0000

    [rest]5-8c

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
   end
   resources :users, only: [:index,:show,:edit,:update]
-  resources :groups, only: [:index, :show, :edit, :create, :update, :new]
+  resources :groups, only: [:index, :show, :edit, :create, :update, :new] do
+    get "join"=>"groups#join"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
